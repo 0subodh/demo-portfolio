adb9053a267641fd3b9ed358b2747aa4dbcac121
Subodh Adhikari <subodh1adhikari@gmail.com>
2025-08-30 00:01:43 +0545

    content centered

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 favouriteMovieGenre("military");
 
 // watermelon, tomato, banana, orange, avocado, blueberry
-favouriteFruit("watermelon");
+favouriteFruit("blueberry");
 
 // light, dark
 favouriteMode("dark");
